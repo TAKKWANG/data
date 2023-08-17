5a4278b8c8ac97e4ff472ed336814a25c6f164bd
TAKKWANG <117281247+TAKKWANG@users.noreply.github.com>
2023-08-17 13:02:55 +0900

    Update link_google_drive.sh

--- misc/link_google_drive.sh
+++ misc/link_google_drive.sh
@@ -19,11 +19,11 @@ if [ $2 == True ]; then
   mv /content/$1/outputs /content/$1/outputs_backup
   rm /content/$1/extensions/sd-dynamic-prompts/wildcards
   mv /content/$1/extensions/sd-dynamic-prompts/wildcards /content/$1/extensions/sd-dynamic-prompts/wildcards_backup
-  ln -s /content/drive/MyDrive/$3/embedding /content/$1/embeddings/google
-  ln -s /content/drive/MyDrive/$3/checkpoint /content/$1/models/Stable-diffusion/google
-  ln -s /content/drive/MyDrive/$3/lora /content/$1/models/Lora/google
-  ln -s /content/drive/MyDrive/$3/lycoris /content/$1/models/LyCORIS/google
-  ln -s /content/drive/MyDrive/$3/hypernetwork /content/$1/models/hypernetworks/google
+  ln -s /content/drive/MyDrive/$3/embedding /content/$1/embeddings
+  ln -s /content/drive/MyDrive/$3/checkpoint /content/$1/models/Stable-diffusion
+  ln -s /content/drive/MyDrive/$3/lora /content/$1/models/Lora
+  ln -s /content/drive/MyDrive/$3/lycoris /content/$1/models/LyCORIS
+  ln -s /content/drive/MyDrive/$3/hypernetwork /content/$1/models/hypernetworks
   ln -s /content/drive/MyDrive/$3/output /content/$1/outputs
   ln -s /content/drive/MyDrive/$3/wildcards /content/$1/extensions/sd-dynamic-prompts/wildcards
 fi
